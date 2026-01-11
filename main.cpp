578ea8ec7a7ddfb071a82438d1abdcda74173868
sebastian75V <villagomezsebastian75@gmail.com>
2026-01-10 19:53:44 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,22 @@
 #include "mainwindow.h"
-#include <QSqlDatabase>
 #include <QApplication>
+#include <QMessageBox>
+#include <QFile>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName("veterinaria.db");
-
-    if (!db.open()) {
-        return -1;
+    // verificar si el archivo existe o crearlo.
+    QFile archivo("pacientes.txt");
+    if (!archivo.exists()) {
+        if (archivo.open(QIODevice::WriteOnly | QIODevice::Text)) {
+            archivo.close(); // Crea el archivo vacío si no existe
+        }
     }
 
     MainWindow w;
     w.show();
     return a.exec();
 }
+
